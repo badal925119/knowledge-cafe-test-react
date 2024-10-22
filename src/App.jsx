d096c0e8eccc68c61e264cd55a7bec0ae21b8ba8
Badal <badalhasanbd5@gmail.com>
2024-10-22 15:57:29 +0600

    adde react and tawlincss

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <h1 className='text-3xl font-bold bg-orange-700  text-white'>Knowladge Cafe</h1>
+     <h5>Helo</h5>
+     <h1>hleo</h1>
+     
+    </>
+  )
+}
+
+export default App
